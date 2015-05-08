60a4ec5756dce0a626fc953aab96d1a41e18f100
horimislime <horimislime@gmail.com>
2015-05-08 14:56:13 +0900

    add osx config

---
+++ osx/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -e ~/Pictures/screenshots ]
+  then
+  mkdir ~/Pictures/screenshots
+fi
+
+# Screenshot directory
+defaults write com.apple.screencapture location ~/Pictures/screenshots
+
+echo "SystemUIServer needs to be restarted. Press [y] to continue."
+read key
+case ${key} in
+  "y" )
+  killall SystemUIServer;;
+  * )
+  echo "Not restarted."
+esac
+
+chflags nohidden ~/Library
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
