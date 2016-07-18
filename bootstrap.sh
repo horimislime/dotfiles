cc7eb116591892d4481ac2917321a22c250680a0
horimislime <horimislime@gmail.com>
2016-07-18 14:42:21 +0900

    cleanup setup files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+# GitHub
 if [ ! -e ~/.ssh/id_rsa.pub ]; then
 	echo "No ssh public key found. Generating..."
 	ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
 	echo "Done."
+
+        cat ~/.ssh/id_rsa.pub |pbcopy
+        echo "SSH public key copied. Register it to Github and press enter."
+        read key
 fi
 
-cat ~/.ssh/id_rsa.pub |pbcopy
-echo "SSH public key copied. Register it to Github and press enter."
-read key
-
+# Homebrew
 if ! which brew >/dev/null; then
-	echo "Installing homebrew..."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 brew update
@@ -23,7 +25,6 @@ else
 	echo "Problem exist on homebrew. Check it after installation by running 'brew doctor'."
 fi
 
-# golang installation
 mkdir ~/.go
 export PATH=$PATH:~/.go/bin
 
