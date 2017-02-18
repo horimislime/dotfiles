0747840d32036188a02f8ba00f6f6df5f451d725
horimislime <horimislime@gmail.com>
2017-02-18 20:53:25 +0900

    Start using fish-shell

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+set -x EDITOR '/usr/local/bin/emacsclient -nw'
+set -x LANG ja_JP.UTF-8
+set -x GOPATH $HOME/.go
+set -x TERM xterm-256color
+
+alias cp 'cp -i'
+alias e 'emacsclient -t'
+alias find 'gfind' # prefer GNU find
+alias grep 'grep --color=auto'
+alias ll 'ls -l'
+alias mv 'mv -i'
+alias o 'open'
+alias oo 'open .'
+alias reload-fish 'source $HOME/.config/fish/config.fish'
+alias sudo 'sudo -E ' # to inherit user defined env-vars
+alias t 'tig status'
