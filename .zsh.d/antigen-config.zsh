7e5bd6e6d39ab8b5a241eb53333129a530ef805e
horimislime <horimislime@gmail.com>
2017-02-18 21:02:54 +0900

    :wave: cleanup

--- .zsh.d/antigen-config.zsh
+++
@@ -1,9 +0,0 @@
-source ~/.zsh.d/antigen/antigen.zsh
-
-antigen use oh-my-zsh
-
-antigen-bundles <<EOBUNDLES
-zsh-users/zsh-syntax-highlighting
-EOBUNDLES
-
-antigen-apply
--- .zsh.d/tools/emacs.zsh
+++
@@ -1,4 +0,0 @@
-## create emacs env file
-perl -wle \
-    'do { print qq/(setenv "$_" "$ENV{$_}")/ if exists $ENV{$_} } for @ARGV' \
-    PATH > ~/.emacs.d/shellenv.el
