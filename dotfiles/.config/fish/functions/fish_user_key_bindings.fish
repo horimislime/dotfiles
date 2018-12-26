add3b6f4ca3023ee9f1f0e4c09dea84b3eef01a2
horimislime <horimislime@gmail.com>
2018-12-26 15:19:58 +0900

    Remove ctrl-f binding from fzf

--- dotfiles/.config/fish/functions/fish_user_key_bindings.fish
+++ dotfiles/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,15 +1,14 @@
 function fish_user_key_bindings
     ### fzf ###
-    bind \cf '__fzf_find_file'
-        bind \cr '__fzf_reverse_isearch'
-        bind \eo '__fzf_cd'
-        bind \eO '__fzf_cd --hidden'
-        if bind -M insert >/dev/null 2>/dev/null
-            bind -M insert \ef '__fzf_find_file'
-            bind -M insert \cr '__fzf_reverse_isearch'
-            bind -M insert \eo '__fzf_cd'
-            bind -M insert \eO '__fzf_cd --hidden'
+    bind \cr '__fzf_reverse_isearch'
+    bind \eo '__fzf_cd'
+    bind \eO '__fzf_cd --hidden'
+    if bind -M insert >/dev/null 2>/dev/null
+       bind -M insert \cr '__fzf_reverse_isearch'
+       bind -M insert \eo '__fzf_cd'
+       bind -M insert \eO '__fzf_cd --hidden'
     end
+
     if set -q FZF_COMPLETE
         bind \t '__fzf_complete'
     end
