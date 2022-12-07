d23258f55a44263702660db87dfb17d3163d5063
horimislime <horimislime@gmail.com>
2022-12-07 14:47:21 +0900

    Update install script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 git submodule update --init --recursive
 
 # set up dotfiles
-dotfiles/setup.sh
+$SCRIPTPATH/dotfiles/setup.sh
 
 # install packages
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    linux/setup.sh
+    $SCRIPTPATH/linux/setup.sh
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    macos/setup.sh
-    homebrew/setup.sh
+    $SCRIPTPATH/macos/setup.sh
+    $SCRIPTPATH/homebrew/setup.sh
 else
     echo "$OSTYPE is not supported."
 fi
