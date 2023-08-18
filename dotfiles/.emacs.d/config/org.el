2a190e4942c1a4c6f16f53088ba53c5fb3a2b690
horimislime <horimislime@gmail.com>
2023-08-18 20:15:01 +0900

    Clean up org config

--- dotfiles/.emacs.d/config/org.el
+++ dotfiles/.emacs.d/config/org.el
@@ -1,7 +1,6 @@
 (use-package pcre2el)
 (use-package org-find-file
-  :load-path "packages/"
-  :after (pcre2el))
+  :load-path "packages/")
 
 (use-package org
   :preface
@@ -13,59 +12,64 @@
     (call-process "~/.homebrew/bin/pngpaste" nil nil nil image-full-path)
     (insert (format "[[./%s]]" image-relative-path))
     (org-redisplay-inline-images))
-
   (defun my/write-to-task-file (content)
     (write-region content
 		  nil "~/.emacs.d/clock-task.txt"
 		  nil 'quiet))
-
   (defun org-set-status-to-doing ()
     (if (org-clocking-p)
 	(org-todo "DOING"))
     (my/write-to-task-file (substring-no-properties org-clock-current-task)))
-
   (defun my/org-empty-current-task-file ()
     (my/write-to-task-file ""))
+  (defun my/create-org-file-with-name ()
+    (let* ((slug (read-string "slug: ")))
+      (format "%s/%s.org" org-directory slug)))
+  (defun my/get-title-from-url (url)
+    (let ((title))
+      (with-current-buffer (url-retrieve-synchronously url)
+	(goto-char (point-min))
+	(re-search-forward "<title>\\([^<]*\\)</title>" nil t 1)
+;	(match-string 1)
+	(setq title (match-string 1))
+	(goto-char (point-min))
+	(re-search-forward "charset=\\([-0-9a-zA-Z]*\\)" nil t 1)
+	(decode-coding-string title (intern (match-string 1))))))
+
+  (defun my/paste-url-with-title ()
+    (interactive)
+    (insert (format "[[%s][%s]]"
+		    (car kill-ring)
+		    (my/get-title-from-url (car kill-ring)))))
+
   :bind
   (("C-c c" . org-capture)
    ("C-c j" . org-journal-new-entry)
    ("C-c o" . org-find-file)
    :map org-mode-map
-   ("C-c C-v" . my/org-screenshot)
-   )
+   ("C-c C-p C-v" . my/org-screenshot)
+   ("C-c C-u C-v" . my/paste-url-with-title))
+
   :hook
   ((org-mode . visual-line-mode)
    (org-clock-in . org-set-status-to-doing)
    (org-clock-out . my/org-empty-current-task-file))
+
+  :init
+  (setq org-directory "~/Google Drive/My Drive/Org"
+	org-daily-tasks-file (format "%s/tasks.org" org-directory))
+
   :custom
-  (org-directory "~/Google Drive/My Drive/Org")
   (org-image-actual-width 900)
   (org-clock-idle-time 60)
   (org-startup-folded 'content)
   (org-startup-with-inline-images t)
   (org-use-speed-commands t)
   (org-todo-keywords '((type "TODO" "WAITING" "DOING"  "|" "DONE")))
-  :config
-  (custom-set-faces
-   '(org-block-begin-line
-     ((((background dark))
-       (:foreground "#669966" :weight bold)) ;; :background "#444444"
-      (t (:foreground "#CC3333" :weight bold)))) ;; :background "#EFEFEF"
-   '(org-block-end-line
-     ((((background dark)) (:foreground "#CC3333" :weight bold))
-      (t (:foreground "#669966" :weight bold))))))
-
-(setq org-capture-templates
-      '(
-        ("n" "Note" plain (file (lambda ()
-                                  (let* ((slug (read-string "slug: ")))
-				    (format "%s/%s.org" org-directory slug))))
-         "#+TITLE: %?\n#+DATE: %T\n#+TAGS: note\n#+EID: %(org-id-uuid)\n\n")
-        ("b" "Blog" plain (file (lambda ()
-                                  (let* ((slug (read-string "slug: ")))
-                                    (concat org-directory "/" (format-time-string "%Y-%m-%d_") slug ".org"))))
-         "#+TITLE: %?\n#+DATE: %T\n#+TZ: %(format-time-string \"%z (%Z)\")\n#+TAGS: draft\n#+EID: %(org-id-uuid)\n\n")
-        ))
+  (org-capture-templates
+   '(("d" "Weekdays TODO" entry (file org-daily-tasks-file) "%[~/.emacs.d/assets/org-templates/weekdays-todo.org]")
+     ("w" "Weekends TODO" entry (file org-daily-tasks-file) "%[~/.emacs.d/assets/org-templates/weekends-todo.org]")
+     ("n" "Note" plain (file my/create-org-file-with-name) "%[~/.emacs.d/assets/org-templates/note.org]"))))
 
 (use-package org-pomodoro
   :custom
@@ -78,6 +82,6 @@
 
 (use-package org-modern
   :hook
-  ((org-mode-hook . org-modern-mode)
-  (org-agenda-finalize-hook . org-modern-agenda)))
+  ((org-mode . org-modern-mode)
+  (org-agenda-finalize . org-modern-agenda)))
 
