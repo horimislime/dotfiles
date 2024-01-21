48945b1c23b23d872b9fce7b03c68224186a3482
horimislime <horimislime@gmail.com>
2024-01-21 23:10:33 +0900

    Distinguish inactive buffer using dim color

--- dotfiles/emacs/config/ui.el
+++ dotfiles/emacs/config/ui.el
@@ -70,3 +70,7 @@
 
 (global-display-line-numbers-mode t)
 
+(use-package auto-dim-other-buffers)
+(add-hook 'after-init-hook (lambda ()
+			     (when (fboundp 'auto-dim-other-buffers-mode)
+			       (auto-dim-other-buffers-mode t))))
